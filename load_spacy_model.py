11d142c378724b50902377285828ad742fc8bbd9
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2024-11-01 10:20:29 +0500

    Update load_spacy_model.py

--- load_spacy_model.py
+++ load_spacy_model.py
@@ -1,13 +1,10 @@
-# load_spacy_model.py
 import spacy
 
 # Try to load the language model
 try:
     nlp = spacy.load("en_core_web_lg")
 except OSError:
-    # If the model is not found, download it
+    print("Model not found. Downloading the model...")
     from spacy.cli import download
     download("en_core_web_lg")
     nlp = spacy.load("en_core_web_lg")  # Load the model after downloading
-
-# Now you can use the nlp object for your NLP tasks
