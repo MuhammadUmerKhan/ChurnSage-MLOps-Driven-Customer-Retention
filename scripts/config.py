342e3227797c932baa337a3de88af1d941c35be0
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-03-20 16:28:54 +0500

    Create config.py

---
+++ scripts/config.py
@@ -0,0 +1,7 @@
+import os
+
+dataset_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Datasets", "TelecomChustomerChurnDataset", "WA_Fn-UseC_-Telco-Customer-Churn.csv"))
+preprocess_data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Datasets", "ProcessedData"))
+scaler_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "models", "scaler.pkl"))
+mlflow_db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "database", "mlflow.db"))
+model_data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Datasets", "ModelData"))
