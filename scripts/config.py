4d2fdcb732c8a073b55ace9c99cb93c5ca5ed9ef
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-03-20 17:57:23 +0500

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,8 @@
 import os
+import dotenv
+dotenv.load_dotenv()
 
+GROK_API_KEY = os.getenv("GROK_API_KEY")
 dataset_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Datasets", "TelecomChustomerChurnDataset", "WA_Fn-UseC_-Telco-Customer-Churn.csv"))
 preprocess_data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Datasets", "ProcessedData"))
 scaler_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "models", "scaler.pkl"))
